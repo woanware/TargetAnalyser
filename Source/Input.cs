020a15d1e664bb3e06e41943fd8a9e92a6f488a9
Mark Woan <markwoan@gmail.com>
2016-12-07 08:43:07 +0000

    Added new Input file

---
+++ Source/Input.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TargetAnalyser
+{
+    public class Input
+    {
+        public string Name;
+        public string FullUrl;
+        public List<string> DataTypes;
+        public List<string> Regex;
+        public List<string> Result;
+        public string HttpMethod;
+        public List<woanware.NameValue> HttpParameters;
+    }
+}
